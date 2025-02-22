ba0202270bbe1111f0309d0789a2db042cc074aa
Peter Wolf <freakcycle@optusnet.com.au>
2025-02-22 10:31:58 -0500

    Add files via upload

--- utils.py
+++ utils.py
@@ -2,13 +2,16 @@
 import re
 
 def	html2text(html):
-	text=re.sub(r'<.[^>]*>',"",html)
-	text=re.sub(r'\'\, b\'\s*'," ",text)
-	text=re.sub(r'=09'," ",text)
+	'''A very simplistic and rough html to text converter'''
+	todelete=[r'<.[^>]*>',r'\'\, b\'\s*',r'=[a-zA-z0-9]{2}\s*',r'\{.[^\}]*\}',r'https?.[^\s]*',r'[a-zA-Z0-9]{20,200}']
+	text=html
+	for el in todelete:
+		text=re.sub(el," ",text)
 	return(text)
 
 
 def inlist(text,thelist):
+	'''Returns true if only if the text is embeded in a list element'''
 	for el in thelist:
 		m=re.search(el,text)
 		try:
@@ -20,6 +23,7 @@ def inlist(text,thelist):
 	return(0)
 
 def	emaildetails(rawemail):
+	'''Returns a list of email details '''
 	fields=["From:.{0,80}<.{0,50}@.{0,50}>\,","Date:.{0,30}\d{2}:\d{2}:\d{2}.{0,30}\,","Subject:.{0,3}=.{5,150}\,"]
 	r2=re.sub("\"","",str(rawemail))
 	r2=re.sub("\'","",r2)
